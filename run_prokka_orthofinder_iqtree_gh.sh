c9b508dff22ce8f3ed2e4c598277ab8602a5489c
José Pablo Iglesias <sir.jpiglesias@gmail.com>
2024-10-26 11:24:26 -0600

    get_homologues.pl integration

--- run_prokka_orthofinder_iqtree_gh.sh
+++ run_prokka_orthofinder_iqtree_gh.sh
@@ -39,6 +39,32 @@ conda activate iqtree # Activate conda environment w/iqtree
 iqtree -m MFP -s ../orthofinder/Results_orthofinder_out_*/Orthologues_orthofinder_out_*/Aligments/SpeciesTreeAlignment.fa -B 1000 -T AUTO -ntmax 30
 
 ################################################################################
-# Future updates
-# Prep files and directories for get_homologues.pl
+# Prep directories for get_homologues.pl
 ################################################################################
+
+cd ../
+mkdir -p get_homologues
+cd get_homologues
+
+ls ../*/*.gbk | while read line; do ln -s $line .; done # Symbolic links
+
+cd ../
+################################################################################
+# Run get_homologues.pl
+################################################################################
+
+conda activate get_homologues # Activate conda environment w/get_homologues
+
+# Define variables
+get_homologues_path = /opt/spack/opt/spack/linux-ubuntu18.04-skylake_avx512/gcc-9.3.0/miniconda3-4.9.2-mdbjxs2dswmwcniwxq2lb6ap7rswdjbh/envs/get_homologues/bin/get_homologues.pl
+pathDir=./get_homologues
+numTaxa=$(ls -1 *.gbk 2>/dev/null | wc -l)
+numCore=20
+
+# Pangenoma
+nohup $get_homologues_path -d "$pathDir" -t 0 -M -e -n "$numCore" > log_gh_pan.txt 2>&1
+
+# Coregenoma
+nohup $get_homologues_path -d "$pathDir" -t "$numTaxa" -M -e -n "$numCore" > log_gh_core_OMCL.txt 2>&1
+nohup $get_homologues_path -d "$pathDir" -t "$numTaxa" -G -e -n "$numCore" > log_gh_core_COG.txt 2>&1
+nohup $get_homologues_path -d "$pathDir" -t "$numTaxa" -e -n "$numCore" > log_gh_core_BDBH.txt 2>&1 &
