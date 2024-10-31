7a8d9ba76b995ab1865520876f275340e1f865ee
José Pablo Iglesias <sir.jpiglesias@gmail.com>
2024-10-31 13:17:27 -0600

    Few fixes

--- core_clusters.sh
+++ core_clusters.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 nohup compare_clusters.pl -d ./GenComp_160824_homologues/C4M0914_f0_149taxa_algBDBH_e1_,\
 ./GenComp_160824_homologues/C4M0914_f0_149taxa_algCOG_e1_,\
-./GenComp_160824_homologues/C4M0914_f0_149taxa_algOMCL_e1_ -o coregenoma -t 149 > log_core_250924_1.txt 2>&1
+./GenComp_160824_homologues/C4M0914_f0_149taxa_algOMCL_e1_ -o coregenoma -t $numTaxa > log_core_250924_1.txt 2>&1
 nohup compare_clusters.pl -d ./GenComp_160824_homologues/C4M0914_f0_149taxa_algBDBH_e1_,\
 ./GenComp_160824_homologues/C4M0914_f0_149taxa_algCOG_e1_,\
-./GenComp_160824_homologues/C4M0914_f0_149taxa_algOMCL_e1_ -o coregenoma -t 149 -n > log_core_250924_2.txt 2>&1 &
+./GenComp_160824_homologues/C4M0914_f0_149taxa_algOMCL_e1_ -o coregenoma -t $numTaxa -n > log_core_250924_2.txt 2>&1 &
 
 
 nohup compare_clusters.pl -d ./GenComp_160824_homologues/C4M0914_f0_0taxa_algOMCL_e1_ -m -o pagenoma > log_pan_250924.txt 2>&1 &
--- run_prokka_orthofinder_iqtree_gh.sh
+++ run_prokka_orthofinder_iqtree_gh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-# PROKKA
+# PROKKA (works with preprocessed .fna files)
 ################################################################################
 module load prokka-master-gcc-9.3.0-j436ckw # Load prokka
 
@@ -39,7 +39,9 @@ cd iqtree
 
 conda activate iqtree # Activate conda environment w/iqtree
 
-iqtree -m MFP -s ../orthofinder/Results_orthofinder_out_*/Orthologues_orthofinder_out_*/Aligments/SpeciesTreeAlignment.fa -B 1000 -T AUTO -ntmax 30
+ls ../orthofinder/Results_orthofinder_out_*/Orthologues_orthofinder_out_*/Aligments/SpeciesTreeAlignment.fa
+
+iqtree -m MFP -s ./SpeciesTreeAlignment.fa -B 1000 -T AUTO -ntmax 30
 
 ################################################################################
 # Prep directories for get_homologues.pl
@@ -71,3 +73,10 @@ nohup $get_homologues_path -d "$pathDir" -t 0 -M -e -n "$numCore" > log_gh_pan.t
 nohup $get_homologues_path -d "$pathDir" -t "$numTaxa" -M -e -n "$numCore" > log_gh_core_OMCL.txt 2>&1
 nohup $get_homologues_path -d "$pathDir" -t "$numTaxa" -G -e -n "$numCore" > log_gh_core_COG.txt 2>&1
 nohup $get_homologues_path -d "$pathDir" -t "$numTaxa" -e -n "$numCore" > log_gh_core_BDBH.txt 2>&1 &
+
+################################################################################
+# Future updates
+# compare_clusters.pl -> coregenoma/pangenoma
+# Scritps R -> matriz_pangenoma_cc.csv
+# matriz_pangenoma_cc.csv -> BayesTraits prep 
+################################################################################
